f3a5d5bccb0352ae25e611fb1561a435f33aa8ad
Anton Lyhin <dotnet@lyhin.com>
2026-03-01 23:09:05 +0300

    L2_T7;

---
+++ c++/02_7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	long long n;
+	cin >> n;
+	cout << n << endl;
+
+	return 0;
+}
+
+/*
+C B 1 B C
+D A 2 A D
+A B 3 B A
+B C 4 C B
+C D 5 D C
+
+A B 1 B A B
+B A 2 A B C
+C B 3 B C D
+B A 4 A B C
+A B 5 B A B
+B C 6 C B A
+*/
